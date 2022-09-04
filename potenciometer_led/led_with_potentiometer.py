aa7df7ea23f5fa1e64ba218d1f1e8189df72bef2
cookieMr <dawid.boisse@gmail.com>
2022-09-04 20:42:29 +0200

    LED with Potentionmeter Schematic - Closes #8

--- potenciometer_led/led_with_potentiometer.py
+++ potenciometer_led/led_with_potentiometer.py
@@ -2,19 +2,19 @@ import RPi.GPIO as gpio
 import time
 
 led_pin = 12
-potenciometer_pin = 14
+potentiometer_pin = 14
 
 gpio.setmode(gpio.BCM)
 gpio.setup(led_pin, gpio.OUT)
-gpio.setup(potenciometer_pin, gpio.IN)
+gpio.setup(potentiometer_pin, gpio.IN)
 gpio.output(led_pin, gpio.LOW)
 
 while True:
-    if (gpio.input(potenciometer_pin) == gpio.HIGH):
-        print("Potenciometer is in ON state.")
+    if (gpio.input(potentiometer_pin) == gpio.HIGH):
+        print("Potentiometer is in ON state.")
         gpio.output(led_pin, gpio.LOW)
     else:
-        print("Potenciometer is in OFF state.")
+        print("Potentiometer is in OFF state.")
         gpio.output(led_pin, gpio.HIGH)
     time.sleep(.2)
 
