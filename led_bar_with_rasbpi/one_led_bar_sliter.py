af97b685a729ab6d274628dc374e82d187c11516
cookieMr <dawid.boisse@gmail.com>
2022-12-04 16:31:42 +0100

    Fix some code issues (8)

--- led_bar_with_rasbpi/one_led_bar_sliter.py
+++ led_bar_with_rasbpi/one_led_bar_sliter.py
@@ -3,28 +3,53 @@ import time
 
 cathodes = [11, 13, 15, 19]
 anodes = [38, 40]
-
-GPIO.cleanup()
-GPIO.setmode(GPIO.BOARD)
-
-for cathode in cathodes:
-    GPIO.setup(cathode, GPIO.OUT)
-    GPIO.output(cathode, GPIO.LOW)
-
-for anode in anodes:
-    GPIO.setup(anode, GPIO.OUT)
-    GPIO.output(anode, GPIO.LOW)
-
 counter = 10
 
-try:
-    while (counter > 0):
-        counter -= 1
+leds = [
+    [anodes[0], cathode[11]],
+    [anodes[0], cathode[13]],
+    [anodes[0], cathode[15]],
+    [anodes[0], cathode[19]],
+    [anodes[1], cathode[11]],
+    [anodes[1], cathode[13]],
+    [anodes[1], cathode[15]],
+    [anodes[1], cathode[19]]
+    ]
 
-        for anode in anodes:
-            GPIO.output(anode, GPIO.HIGH)
-            time.sleep(0.3)
-            GPIO.output(anode, GPIO.LOW)
+def setup():
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+
+    for cathode in cathodes:
+        GPIO.setup(cathode, GPIO.OUT)
+        GPIO.output(cathode, GPIO.LOW)
+
+    for anode in anodes:
+        GPIO.setup(anode, GPIO.OUT)
+        GPIO.output(anode, GPIO.LOW)
+
+def turn_on_one_led(the_anode: Int, the_cathode: Int):
+    for anode in anodes:
+        GPIO.output(anode, GPIO.LOW if anode != the_anode else GPIO.HIGH)
+    for cathode in cathodes:
+        GPIO.output(anode, GPIO.HIGH if cathode != the_cathode else GPIO.LOW)
+
+def turn_all_off():
+    or anode in anodes:
+        GPIO.output(anode, GPIO.LOW)
+    for cathode in cathodes:
+        GPIO.output(anode, GPIO.LOW)
+
+try:
+    setup()
+    while (counter > 0):
+        for led in leds:
+            turn_on_one_led(led[0], led[1])
+            time.sleep(0.1)
+            turn_all_off()
+
+except KeyboardInterrupt:
+    turn_all_off()
 
 finally:
     GPIO.cleanup()
