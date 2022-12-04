bb18957a17eb68b2bfebe6d5e790d4c7efeca3f2
cookieMr <dawid.boisse@gmail.com>
2022-12-04 16:18:27 +0100

    Fix some code issues (5)

--- led_bar_with_rasbpi/one_led_bar_sliter.py
+++ led_bar_with_rasbpi/one_led_bar_sliter.py
@@ -8,26 +8,23 @@ GPIO.setmode(GPIO.BOARD)
 
 for cathode in cathodes:
     GPIO.setup(cathode, GPIO.OUT)
-    GPIO.output(cathode, 0)
+    GPIO.output(cathode, GPIO.LOW)
 
 for anode in anodes:
     GPIO.setup(anode, GPIO.OUT)
-    GPIO.output(anode, 0)
+    GPIO.output(anode, GPIO.LOW)
 
 counter = 10
 
 try:
     while (counter > 0):
-        counter = counter - 1
+        counter -= 1
 
-        for c in cathodes:
-            GPIO.output(c, 1)
+        for cathode in cathodes:
+            GPIO.output(cathode, GPIO.HIGH)
             time.sleep(0.3)
-            GPIO.output(c, 0)
+            GPIO.output(cathode, GPIO.LOW)
 
-        time.sleep(0.3)
-
-except KeyboardInterrupt:
+finally:
     GPIO.cleanup()
-GPIO.cleanup()
 
